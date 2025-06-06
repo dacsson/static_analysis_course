d1183068fff2bb3e861518e0db4dda648273111f
dacsson <sda20036@gmail.com>
2025-06-06 20:15:16 +0300

    Task3. Flow-sensetive analysisies

--- src/tip/analysis/LiveVarsAnalysis.scala
+++ src/tip/analysis/LiveVarsAnalysis.scala
@@ -3,6 +3,7 @@ package tip.analysis
 import tip.ast._
 import tip.lattices._
 import tip.ast.AstNodeData.DeclarationData
+import tip.ast.AstOps.AstOp
 import tip.solvers._
 import tip.cfg._
 
@@ -25,15 +26,19 @@ abstract class LiveVarsAnalysis(cfg: IntraproceduralProgramCfg)(implicit declDat
       case _: CfgFunExitNode => lattice.sublattice.bottom
       case r: CfgStmtNode =>
         r.data match {
-          case cond: AExpr => ??? //<--- Complete here
+          case cond: AExpr => s ++ cond.appearingIds
           case as: AAssignStmt =>
             as.left match {
-              case id: AIdentifier => ??? //<--- Complete here
+              case id: AIdentifier =>
+                // [[x = E]] = JOIN(n) \ {x} U vars(E) -> remove from set, add included expres
+                s - id ++ as.right.appearingIds
               case _ => ???
             }
-          case varr: AVarStmt => ??? //<--- Complete here
-          case ret: AReturnStmt => ??? //<--- Complete here
-          case out: AOutputStmt => ??? //<--- Complete here
+          case varr: AVarStmt =>
+            // [[var x]] = JOIN(n) \ {x} -> remove from set
+            s -- varr.appearingIds
+          case ret: AReturnStmt => s ++ ret.exp.appearingIds
+          case out: AOutputStmt => s ++ out.exp.appearingIds
           case _ => s
         }
       case _ => s
